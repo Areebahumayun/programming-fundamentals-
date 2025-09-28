1904198bcab617006840614df74865da8235d758
Areebahumayun <areebahmn@gmail.com>
2025-09-28 10:24:16 -0700

    Update pf 2.cpp

--- pf 2.cpp
+++ pf 2.cpp
@@ -1,5 +1,25 @@
-#include<stdio.h>
-int main ()
-{
+ #include <stdio.h>
+
+int main() {
+    unsigned char num;
+
+    printf("Enter an 8-bit number (0-255): ");
+    scanf("%hhu", &num);
+
+    unsigned char mask_4th_bit = 1 << 3; 
+    unsigned char mask_7th_bit = 1 << 6; 
+
+    if ((num & mask_4th_bit) && (num & mask_7th_bit)) {
+        printf("Both 4th and 7th bits are ON.\n");
+        num &= ~mask_4th_bit; 
+        num &= ~mask_7th_bit; 
+        printf("Bits turned OFF. New number: %hhu\n", num);
+    } else {
+        printf("Either 4th or 7th bit (or both) are OFF.\n");
+    }
+
+    return 0;
+}
 	
+
 }
